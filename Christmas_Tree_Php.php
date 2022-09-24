67cb2a06f33beec91c46ba5a93dfc5cf07a13f82
Suparna_Das <105983242+SDisback@users.noreply.github.com>
2022-09-24 13:22:00 +0530

    Update and rename README.md to Christmas_Tree_Php.php

---
+++ Christmas_Tree_Php.php
@@ -0,0 +1,79 @@
+<h4>☆ Coded by Suparna Das <br>
+
+☆ Program to make Christmas             
+Tree in PHP <br>
+
+☆ Thanks for watching
+</h4>
+
+<h1>Christmas Tree ♧</h1>
+
+<?php
+    
+    $size = 5;
+    for($i = 0; $i < $size; $i++) {
+        
+        for($j = 0; $j < $size - $i - 1; $j++) {
+            echo "&nbsp;&nbsp;";
+        }
+        
+        for($k = 0; $k < 2 * $i + 1; $k++) {
+            echo "*";
+        }
+        echo "<br>";
+    }
+
+    
+    $size = 5;
+    for($i = 0; $i < $size; $i++) {
+        
+        for($j = 0; $j < $size - $i - 1; $j++) {
+            echo "&nbsp;&nbsp;";
+        }
+        
+        for($k = 0; $k < 2 * $i + 1; $k++) {
+            echo "*";
+        }
+        echo "<br>";
+    }
+
+    
+    $size = 5;
+    for($i = 0; $i < $size; $i++) {
+        
+        for($j = 0; $j < $size - $i - 1; $j++) {
+            echo "&nbsp;&nbsp;";
+        }
+        
+        for($k = 0; $k < 2 * $i + 1; $k++) {
+            echo "*";
+        }
+        echo "<br>";
+    }
+        
+    // 4th part
+    $size = 5;
+    for($i = 0; $i < $size; $i++) {
+        
+        for($j = 0; $j < $size - $i - 1; $j++) {
+            echo "&nbsp;&nbsp;";
+        }
+        
+        for($k = 0; $k < 2 * $i + 1; $k++) {
+            echo "*";
+        }
+        echo "<br>";
+    }
+
+
+        
+    $h = 3;
+    for($k=0; $k<=$h;$k++){
+        echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; | |";
+        
+        echo "<br>";
+    }
+
+
+
+?>
